a35a083537555610ce032f1f5a7dbfe132a9c55d
Yamini <154418425+Kaamaakshi@users.noreply.github.com>
2024-10-16 18:52:52 +0530

    Update LoginController.java

--- springboot-oauth/src/main/java/org/example/springbootoauth/controller/LoginController.java
+++ springboot-oauth/src/main/java/org/example/springbootoauth/controller/LoginController.java
@@ -1,36 +1,3 @@
-//package org.example.springbootoauth.controller;
-//
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//@Controller
-//public class LoginController {
-//
-//    // Handle GET request for login page
-//    @GetMapping("/oauth/login")
-//    public String login() {
-//        return "login"; // Return the login view
-//    }
-//
-//    // Handle GET request for successful login
-//    @GetMapping("/loginSuccess")
-//    public String profile(OAuth2AuthenticationToken token, Model model) {
-//        model.addAttribute("name",token.getPrincipal().getAttribute("name"));
-//        model.addAttribute("email",token.getPrincipal().getAttribute("email"));
-//        model.addAttribute("photo",token.getPrincipal().getAttribute("photo"));
-//        return "loginSuccess"; // Return the login success view
-//    }
-//
-//    // Handle GET request for logout success
-//    @GetMapping("/logout")
-//    public String logout() {
-//        return "logout"; // Return the logout success view
-//    }
-//}
 package org.example.springbootoauth.controller;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
